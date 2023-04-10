ddc0dbb014f4f35789b0e8d69aadbe61e0dff294
Nguyễn Văn Dũng <37447296+vandung3101@users.noreply.github.com>
2023-04-10 09:48:25 +0000

    uni proj

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 import requests
 from spacy_streamlit import visualize_parser, visualize_ner, visualize_textcat
 import spacy
-import deplacy
 
 # os.system("/home/appuser/venv/bin/python -m spacy download en_core_web_sm")
 st.title("Demo of text to text model")
@@ -24,15 +23,31 @@ def text2text(sentence):
     response = query(payload)
     return response
 
-en_sentence = st.text_input("Enter your en sentence:")
-if st.button("Generate en sentence"):
-    answer = text2text(en_sentence)
-    doc = nlp(en_sentence)
-    visualize_parser(doc)
-    visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
-        
+# en_sentence = st.text_input("Enter your en sentence:")
+# if st.button("Generate"):
+#     answer = text2text(en_sentence)
+#     doc = nlp(en_sentence)
+#     visualize_parser(doc)
+#     visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
 
-vi_sentence = st.text_input("Enter your vi sentence:")
-if st.button("Generate vi sentence"):
-    answer = text2text(vi_sentence)
-    st.write(answer)
+# vi_sentence = st.text_input("Enter your vi sentence:")
+# if st.button("Generate"):
+#     answer = text2text(vi_sentence)
+#     st.write(answer)
+
+# I want to have a sidebar to select the language, then the input box and button will be displayed based on the language selected. How can I do that?
+st.sidebar.title("Select language")
+language = st.sidebar.selectbox("Language", ["English", "Vietnamese"])
+
+if language == "English":
+    en_sentence = st.text_input("Enter your en sentence:")
+    if st.button("Generate"):
+        answer = text2text(en_sentence)
+        doc = nlp(en_sentence)
+        visualize_parser(doc)
+        visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
+else:
+    vi_sentence = st.text_input("Enter your vi sentence:")
+    if st.button("Generate"):
+        answer = text2text(vi_sentence)
+        st.write(answer)
