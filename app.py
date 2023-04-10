67155e2fdfcfbd4630c57f358ce4f73bc5db3d5d
Nguyễn Văn Dũng <37447296+vandung3101@users.noreply.github.com>
2023-04-10 09:29:36 +0000

    uni proj

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 import requests
 from spacy_streamlit import visualize_parser, visualize_ner, visualize_textcat
 import spacy
+import deplacy
 
 # os.system("/home/appuser/venv/bin/python -m spacy download en_core_web_sm")
 st.title("Demo of text to text model")
@@ -29,7 +30,7 @@ if st.button("Generate en sentence"):
 	doc = nlp(en_sentence)
 	visualize_parser(doc)
 	visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
-	visualize_textcat(doc)
+    deplacy.serve(doc)
 
 vi_sentence = st.text_input("Enter your vi sentence:")
 if st.button("Generate vi sentence"):
