67723ce48673c12a35e11ca4cc110612885b9068
Nguyễn Văn Dũng <37447296+vandung3101@users.noreply.github.com>
2023-04-10 15:04:50 +0000

    update app.py

--- app.py
+++ app.py
@@ -2,13 +2,12 @@
 import os
 import streamlit as st
 import requests
-from spacy_streamlit import visualize_parser, visualize_ner, visualize_textcat
+from spacy_streamlit import visualize_parser, visualize_ner
 import spacy
 from spacy import displacy
-import deplacy
 
 os.system("/home/appuser/venv/bin/python -m spacy download en_core_web_sm")
-st.title("Demo of text to text model")
+st.title("NLP Application")
 
 VI_MODEL_API_URL = "https://api-inference.huggingface.co/models/ihgn/similar-questions"
 EN_MODEL_API_URL = "https://api-inference.huggingface.co/models/vandung/t5-para"
@@ -25,32 +24,23 @@ def text2text(sentence):
     response = query(payload)
     return response
 
-# en_sentence = st.text_input("Enter your en sentence:")
-# if st.button("Generate"):
-#     answer = text2text(en_sentence)
-#     doc = nlp(en_sentence)
-#     visualize_parser(doc)
-#     visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
-
-# vi_sentence = st.text_input("Enter your vi sentence:")
-# if st.button("Generate"):
-#     answer = text2text(vi_sentence)
-#     st.write(answer)
-
-# use sidebar to select language, then show the corresponding input box, and the corresponding model, and the corresponding visualization
-
 st.sidebar.title("Select language")
 language = st.sidebar.selectbox("Language", ["English", "Vietnamese"])
 
 if language == "English":
-    en_sentence = st.text_input("Enter your en sentence:")
+    en_sentence = st.text_input("Enter your sentence:")
     if st.button("Generate"):
         answer = text2text(en_sentence)
         doc = nlp(en_sentence)
-        visualize_parser(doc, displacy_options={"Compactttttt": "True", "bg": "#09a3d5", "color": "white", "font": "Source Sans Pro", "collapse_phrases": True})
+        visualize_parser(doc, displacy_options={"Compact": True, "bg": "#09a3d5", "color": "white", "font": "Source Sans Pro", "collapse_phrases": True})
         visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
+        st.write(answer)
 else:
     vi_sentence = st.text_input("Enter your vi sentence:")
     if st.button("Generate"):
         answer = text2text(vi_sentence)
+        doc = nlp(vi_sentence)
+        visualize_parser(doc, displacy_options={
+                         "Compact": True, "bg": "#09a3d5", "color": "white", "font": "Source Sans Pro", "collapse_phrases": True})
+        visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
         st.write(answer)
