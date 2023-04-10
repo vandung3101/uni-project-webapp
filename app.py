1783d99e0e2f3f9c5bf6c196021ba6ebfe6c7fca
Nguyễn Văn Dũng <37447296+vandung3101@users.noreply.github.com>
2023-04-10 08:10:03 +0000

    use codespace

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # write a beutiful streamlit app that call to text to text model of huggingface
 # and display the result
 import streamlit as st
-# from transformers import pipeline
-
-st.title("Demo of text to text model")
-
-
 import requests
+import spacy_streamlit
+import os
+
+os.system("python -m spacy download en_core_web_sm")
+st.title("Demo of text to text model")
 
 VI_MODEL_API_URL = "https://api-inference.huggingface.co/models/ihgn/similar-questions"
 EN_MODEL_API_URL = "https://api-inference.huggingface.co/models/vandung/t5-para"
@@ -21,6 +21,11 @@ def text2text(sentence):
 	response = query(payload)
 	return response
 
+
+models = ["en_core_web_sm"]
+default_text = "Sundar Pichai is the CEO of Google."
+spacy_streamlit.visualize(models, default_text)
+
 en_sentence = st.text_input("Enter your en sentence:")
 if st.button("Generate en sentence"):
     answer = text2text(en_sentence)
@@ -30,5 +35,3 @@ vi_sentence = st.text_input("Enter your vi sentence:")
 if st.button("Generate vi sentence"):
     answer = text2text(vi_sentence)
     st.write(answer)
-
-
