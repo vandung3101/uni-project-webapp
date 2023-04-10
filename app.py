d73683a5c3448e79a035da0bd23c2d24b359032e
Nguyễn Văn Dũng <37447296+vandung3101@users.noreply.github.com>
2023-04-10 09:35:15 +0000

    uni proj

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ def query(payload, API_URL=EN_MODEL_API_URL):
     return response.json()
 
 def text2text(sentence):
-	payload = {"inputs": sentence}
-	response = query(payload)
-	return response
+    payload = {"inputs": sentence}
+    response = query(payload)
+    return response
 
 en_sentence = st.text_input("Enter your en sentence:")
 if st.button("Generate en sentence"):
-	answer = text2text(en_sentence)
-	doc = nlp(en_sentence)
-	visualize_parser(doc)
+    answer = text2text(en_sentence)
+    doc = nlp(en_sentence)
+    visualize_parser(doc)
     deplacy.serve(doc)
-	visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
+    visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
         
 
 vi_sentence = st.text_input("Enter your vi sentence:")
